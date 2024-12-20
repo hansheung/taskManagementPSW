86fd0ca848df24b865846b75270c04e4931ce40a
Hansheung <hansheung@gmail.com>
2024-12-20 18:04:37 +0800

    Added Edit/Update functionality

--- src/ManageItem.jsx
+++ src/ManageItem.jsx
@@ -21,3 +21,23 @@ export const DeleteItem = (id) => {
     JSON.stringify(currentTasks.filter((ct) => ct.id !== id))
   );
 };
+
+export const UpdateItem = (id, updatedData) =>{
+  const _currentTasks = localStorage.getItem("tasks")
+  let currentTasks = JSON.parse(_currentTasks);
+  
+  const taskIndex = currentTasks.findIndex(task => task.id === id);
+
+  if (taskIndex !== -1) {
+    
+    currentTasks[taskIndex] = {
+      ...currentTasks[taskIndex],
+      name: updatedData.name || tasks[taskIndex].name,
+      description: updatedData.description || tasks[taskIndex].description,
+      category: updatedData.category || tasks[taskIndex].category
+    };
+    
+    // Save back to localStorage
+    localStorage.setItem("tasks", JSON.stringify(currentTasks));
+  }
+}
